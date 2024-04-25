14b9f3cb31972161254c85f4f61a3bb99a5f85da
Eric Liu <eric@gameinstitute.cn>
2024-04-25 09:01:48 +0800

    去除设置面板，增加鼠标输入等

--- Frontend/implementations/typescript/src/player.ts
+++ Frontend/implementations/typescript/src/player.ts
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-import { Config, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.4';
-import { Application, PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.4';
+import { Config, Flags, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.4';
+import { Application, UIOptions, PanelConfiguration, UIElementCreationMode, PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.4';
 const PixelStreamingApplicationStyles =
     new PixelStreamingApplicationStyle();
 PixelStreamingApplicationStyles.applyStyleSheet();
@@ -16,14 +16,42 @@ document.body.onload = function() {
 	// Logger.SetLoggerVerbosity(10);
 
 	// Create a config object
-	const config = new Config({ useUrlParams: true });
+	const config = new Config({
+		initialSettings: {
+			AutoPlayVideo: true,
+			AutoConnect: true,
+			StartVideoMuted: true,
+			WaitForStreamer: true,
+		},
+		useUrlParams: true,
+		});
+	config.setFlagEnabled(Flags.HoveringMouseMode, true);
+	config.setFlagEnabled(Flags.FakeMouseWithTouches, true);
+	// config.setFlagEnabled(Flags.MouseInput, false);
+	// config.setFlagEnabled(Flags.KeyboardInput, false);
+	// config.setFlagEnabled(Flags.TouchInput, false);
+	// config.setFlagEnabled(Flags.GamepadInput, false);
+	// config.setFlagEnabled(Flags.XRControllerInput, false);
 
 	// Create a Native DOM delegate instance that implements the Delegate interface class
 	const stream = new PixelStreaming(config);
 
 	const application = new Application({
 		stream,
-		onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode)
+		onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode),
+		settingsPanelConfig: { 
+			isEnabled : false,
+			visibilityButtonConfig : { creationMode : UIElementCreationMode.Disable }
+		},
+		statsPanelConfig:
+		{
+			isEnabled : false,
+			visibilityButtonConfig : { creationMode : UIElementCreationMode.Disable }
+		},
+		fullScreenControlsConfig:
+		{
+			creationMode : UIElementCreationMode.Disable
+		}
 	});
 	// document.getElementById("centrebox").appendChild(application.rootElement);
 	document.body.appendChild(application.rootElement);
